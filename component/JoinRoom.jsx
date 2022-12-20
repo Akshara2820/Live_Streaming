128389dac6270fa68147f93170e6648570bc3b11
Frontend1 <fdev1.delta4@gmail.com>
2022-12-20 15:08:52 +0530

    first commit

---
+++ component/JoinRoom.jsx
@@ -0,0 +1,69 @@
+import { React, useState } from "react";
+import { useHMSActions } from "@100mslive/react-sdk";
+
+function JoinRoom() {
+  const ENDPOINT = process.env.REACT_APP_TOKEN_ENDPOINT;
+  const ROOM_ID = process.env.REACT_APP_ROOM_ID;
+
+  const [selectedRole, setSelectedRole] = useState("broadcaster");
+  const hmsActions = useHMSActions();
+  const [inputValues, setInputValues] = useState({
+    name: "",
+    token: "",
+    role:'',
+  });
+
+  const handleInputChange = (e) => {
+    setInputValues((prevValues) => ({
+      ...prevValues,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    hmsActions.join({
+      userName: inputValues.name,
+      authToken: inputValues.token,
+      role:inputValues.role
+    });
+  };
+
+  return (
+    <form className="join" onSubmit={handleSubmit}>
+     <h2>Join Stream</h2>
+      <input
+        required
+        value={inputValues.name}
+        onChange={handleInputChange}
+        id="name"
+        type="text"
+        name="name"
+        placeholder="Your name"
+      />
+      <input
+        required
+        value={inputValues.token}
+        onChange={handleInputChange}
+        id="token"
+        type="text"
+        name="token"
+        placeholder="Auth token"
+      />
+
+      <select
+        type="text"
+        required
+        value={inputValues.role}
+        onChange={handleInputChange}
+        placeholder="Select Role"
+      >
+        <option>broadcaster</option>
+        <option>hls-viewer</option>
+      </select>
+      <button className='join-btn'>Join</button>
+    </form>
+  );
+}
+
+export default JoinRoom;
